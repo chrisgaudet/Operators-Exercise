6720f144c855ef268fa758fd1aa8671ea5df2342
Chris Gaudet <chrisgaudet9@gmail.com>
2024-07-17 20:12:44 -0500

    finished first exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,32 @@
 {
     public class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+             int a = 17;
+
+             int b = 4;
+
+             int sum = a + b;
+             Console.WriteLine($"{a}+{b} is {sum}");
+
+             int subtract = a - b;
+             Console.WriteLine($"{a}-{b} is {subtract}");
+
+             int multiply = a * b;
+             Console.WriteLine($"{a}*{b} is {multiply}");
+
+             int quotient = a / b;
+             Console.WriteLine($"{a}/{b} is {quotient}");
+
+             int remainder = a % b;
+             Console.WriteLine($"{a}%{b} is {remainder}");
+
+             Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
         }
+        
     }
 }
+    
+
