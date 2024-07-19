bd894035deb08dadcf14339f0110f1900b325d58
Chris Gaudet <chrisgaudet9@gmail.com>
2024-07-18 21:19:54 -0500

    finished second exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,32 +2,42 @@
 {
     public class Program
     {
-        
+
         static void Main(string[] args)
         {
-             int a = 17;
+            int a = 17;
 
-             int b = 4;
+            int b = 4;
 
-             int sum = a + b;
-             Console.WriteLine($"{a}+{b} is {sum}");
+            int sum = a + b;
+            Console.WriteLine($"{a}+{b} is {sum}");
 
-             int subtract = a - b;
-             Console.WriteLine($"{a}-{b} is {subtract}");
+            int subtract = a - b;
+            Console.WriteLine($"{a}-{b} is {subtract}");
 
-             int multiply = a * b;
-             Console.WriteLine($"{a}*{b} is {multiply}");
+            int multiply = a * b;
+            Console.WriteLine($"{a}*{b} is {multiply}");
 
-             int quotient = a / b;
-             Console.WriteLine($"{a}/{b} is {quotient}");
+            int quotient = a / b;
+            Console.WriteLine($"{a}/{b} is {quotient}");
 
-             int remainder = a % b;
-             Console.WriteLine($"{a}%{b} is {remainder}");
+            int remainder = a % b;
+            Console.WriteLine($"{a}%{b} is {remainder}");
 
-             Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+            AreaOfACircle();
         }
-        
+
+        static void AreaOfACircle()
+        { 
+
+            double radius = double.Parse(Console.ReadLine());
+
+            double result = Math.PI * Math.Pow(radius, 2);
+            Console.WriteLine($"What is the radius {radius} of my cirlcle? {result}");
+        }
     }
 }
-    
+
 
